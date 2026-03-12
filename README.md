c758faabc29a040453fff8a81e9f7ce960bb002e
SamuelK0nrad <samuel.ko702@gmail.com>
2026-03-12 21:11:28 +0100

    add simple db model

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 
 Goal: Highly customisable API for a blog service, integration with [ory auth provider](https://github.com/Samuelk0nrad/docker-ory), and dynamic blog posts with MDX
+
+
+
+## TODO:
+
+- [ ] ---
--- backend/db/migration/00001_create_blog.sql
+++ backend/db/migration/00001_create_blog.sql
@@ -1,10 +1,42 @@
 -- +goose Up
-CREATE TABLE blog_metadata (
-  id SERIAL PRIMARY KEY,
-  title TEXT,
+CREATE TABLE blog_post (
+  id          SERIAL PRIMARY KEY,
+  title       TEXT not null,
+  slug        TEXT unique not null,
   description TEXT,
-  author TEXT
+  author      TEXT not null,
+
+  created_by TEXT not null,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+
+  updated_by TEXT,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tag (
+  id          SERIAL PRIMARY KEY,
+  title       TEXT not null,
+  slug        TEXT unique not null,
+  description TEXT,
+
+  created_by TEXT not null,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+
+  updated_by TEXT,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE blog_tag (
+  tag_id  INTEGER not null,
+  blog_id INTEGER not null,
+
+  CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tag(id) ON DELETE CASCADE,
+  CONSTRAINT fk_blog FOREIGN KEY(blog_id) REFERENCES blog_post(id) ON DELETE CASCADE,
+
+  PRIMARY KEY (tag_id, blog_id)
 );
 
 -- +goose Down
-DROP TABLE blog_metadata;
+DROP TABLE blog_tag;
+DROP TABLE tag;
+DROP TABLE blog_post;
